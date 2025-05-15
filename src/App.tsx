3823aa7a781b806bd421e3d3d61876f8d9d922a4
sadke8465 <58910781+sadke8465@users.noreply.github.com>
2025-05-15 13:33:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import React from "react";
 import { MeshBackdrop } from "./MeshBackdrop";
+import NowPlayingArt from "./NowPlayingArt";
 
 export default function App() {
   return (
     <>
-      <h1 style={{color:"white",textAlign:"center",marginTop:"40vh"}}>
-        Album-Art Mesh (placeholder)
-      </h1>
+      {/* Foreground UI: album-art, zoom effects, etc. */}
+      <NowPlayingArt />
+
+      {/* Background animated mesh gradient */}
       <MeshBackdrop />
     </>
   );
