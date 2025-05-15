34fa8452960874ba13e3b9b2efdac7f4cbd5bb9e
sadke8465 <58910781+sadke8465@users.noreply.github.com>
2025-05-15 12:12:16 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { MeshBackdrop } from "./MeshBackdrop";
+
+export default function App() {
+  return (
+    <>
+      <h1 style={{color:"white",textAlign:"center",marginTop:"40vh"}}>
+        Album-Art Mesh (placeholder)
+      </h1>
+      <MeshBackdrop />
+    </>
+  );
+}
